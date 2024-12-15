a67ef433ef633371e38246ae962ca8d5661e1694
mahmoud-homosany <mahmoudhomosany6@gmail.com>
2024-12-15 14:39:18 +0200

    addDashboardLink

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseURL: 'https://localhost:7016/api',
   baseuRL: 'https://walmart-api.runasp.net/api',
-  url: "https://localhost:7028"
+  url: "https://walmart-dashboard.runasp.net"
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   baseURL: 'https://localhost:7016/api',
   baseuRL: 'https://walmart-api.runasp.net/api',
-  url: "https://localhost:7028"
+  url: "https://walmart-dashboard.runasp.net"
 };
